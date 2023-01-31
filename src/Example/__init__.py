9d66da6fb6062b4d52636bbf14a6dc7e59848b8b
James Frost <james.frost@metoffice.gov.uk>
2023-01-31 14:59:53 +0000

    minimise example code

--- src/Example/__init__.py
+++ src/Example/__init__.py
@@ -3,3 +3,8 @@ Example package to demonstrate pytest and CI tooling.
 
 This __init__.py file is needed for making this into a valid package.
 """
+from .example import hello_world
+
+def main():
+    hello_world()
+    return 0
